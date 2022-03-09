f0e2e6850b42a0b9590246100434e74a158bcc72
try <ertert>
2022-03-10 00:42:27 +0100

    Nav Bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DRNavbar from './components/DRNavbar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <DRNavbar injectFirst>
+    {/* <Demo /> */}
+  </DRNavbar>,
   </React.StrictMode>,
   document.getElementById('root')
 );
