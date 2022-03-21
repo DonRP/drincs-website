7c993915108e010307cf4d608004aba0715cac89
Don Renpy <67595890+DonRP@users.noreply.github.com>
2022-03-21 19:15:55 +0100

    AuthService

---
+++ src/shared/DefaultValues.js
@@ -0,0 +1,4 @@
+export const LISTA_TOKEN = [
+  { nameToken: "Crowdin", token: "341fab5894e776804f96112981befc4b60aa1f7dc0c6767f9ab7bf8cd00f99d83820d605b95de5b2" },
+  { nameToken: "Twitter", token: "AAAAAAAAAAAAAAAAAAAAAGiWaQEAAAAAiJC5xwir2xa6Nv2%2FkonMB2f5cps%3D8Bt1aY5cspkPryH8Ob9QgI54QENFN6LPR1qtu6uvv2mPVpvrbh" },
+]
