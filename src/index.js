11026d15826d2a3c99061c0728ea33c743aaafdb
Don Renpy <67595890+DonRP@users.noreply.github.com>
2022-03-27 18:28:27 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const functions = require('firebase-functions');
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
 ReactDOM.render(
     <App />
     , document.getElementById('root')
