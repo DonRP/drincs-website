705007aa04e7fd91d7bf05fbc522a23d90d96a9a
Black Ram <67595890+DonRP@users.noreply.github.com>
2023-08-23 13:02:52 +0200

    add button

--- src/page/Wiki.tsx
+++ src/page/Wiki.tsx
@@ -1,4 +1,5 @@
-import { Grid } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { Fab, Grid } from "@mui/material";
 import MarkdownCard from "components/MarkdownCard";
 import { ElementContent } from "react-markdown/lib/ast-to-react";
 import { analyticPageView } from "utility/Analytics";
@@ -10,7 +11,6 @@ type WikiProps = {
 }
 
 function Wiki(props: WikiProps) {
-    analyticPageView("Wiki", props.urlRepo + " - " + props.routeLink)
 
     const { urlRepo, sidebar = "_Sidebar.md", routeLink } = props
     const transformLinkUri = (href: string, children: Array<ElementContent>, title: string | null) => {
@@ -22,51 +22,69 @@ function Wiki(props: WikiProps) {
         }
     }
     const queryParameters = new URLSearchParams(window.location.search)
-    const route = queryParameters.get("route")
+    const route = queryParameters.get("route") || "Home"
+
+    analyticPageView("Wiki", props.urlRepo + " - " + route)
 
     return (
-        <Grid
-            container
-            direction={{ xs: "column", md: "row" }}
-            justifyContent="center"
-            alignItems={{ xs: "stretch", md: "flex-start" }}
-            spacing={0}
-            marginTop={5}
-            marginBottom={5}
-        >
-            <Grid item xs={1}
-                sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
-            />
-            <Grid item xs={2}
-                sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+        <>
+            <Fab
+                variant="extended"
+                color="primary"
+                aria-label="add"
+                onClick={() => {
+                    window.open(`https://github.com/${props.urlRepo}/wiki/${route}/_edit`);
+                }}
+
+                sx={{
+                    position: "fixed",
+                    right: "3%",
+                    bottom: "10%",
+                }}>
+                <EditIcon />
+            </Fab>
+            <Grid
+                container
+                direction={{ xs: "column", md: "row" }}
+                justifyContent="center"
+                alignItems={{ xs: "stretch", md: "flex-start" }}
+                spacing={0}
+                marginTop={5}
+                marginBottom={5}
             >
-                <MarkdownCard
-                    minWidth={150}
-                    markdownLink={`https://raw.githubusercontent.com/wiki/${urlRepo}/${sidebar}`}
-                    transformLinkUri={transformLinkUri}
+
+                <Grid item xs={1}
+                    sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+                />
+                <Grid item xs={2}
+                    sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+                >
+                    <MarkdownCard
+                        minWidth={150}
+                        markdownLink={`https://raw.githubusercontent.com/wiki/${urlRepo}/${sidebar}`}
+                        transformLinkUri={transformLinkUri}
+                    />
+                </Grid>
+                <Grid item xs={7}>
+                    <MarkdownCard
+                        markdownLink={`https://raw.githubusercontent.com/wiki/${urlRepo}/${route}.md`}
+                        transformLinkUri={transformLinkUri}
+                    />
+                </Grid>
+                <Grid item xs={12}
+                    sx={{ flexGrow: 1, display: { xs: 'grid', md: 'none' } }}
+                >
+                    <MarkdownCard
+                        minWidth={150}
+                        markdownLink={`https://raw.githubusercontent.com/wiki/${urlRepo}/${sidebar}`}
+                        transformLinkUri={transformLinkUri}
+                    />
+                </Grid>
+                <Grid item xs={2}
+                    sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
                 />
             </Grid>
-            <Grid item xs={7}>
-                <MarkdownCard
-                    markdownLink={route ?
-                        `https://raw.githubusercontent.com/wiki/${urlRepo}/${route}.md` :
-                        `https://raw.githubusercontent.com/wiki/${urlRepo}/Home.md`}
-                    transformLinkUri={transformLinkUri}
-                />
-            </Grid>
-            <Grid item xs={12}
-                sx={{ flexGrow: 1, display: { xs: 'grid', md: 'none' } }}
-            >
-                <MarkdownCard
-                    minWidth={150}
-                    markdownLink={`https://raw.githubusercontent.com/wiki/${urlRepo}/${sidebar}`}
-                    transformLinkUri={transformLinkUri}
-                />
-            </Grid>
-            <Grid item xs={2}
-                sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
-            />
-        </Grid>
+        </>
     );
 };
 
