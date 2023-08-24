7238ecc77721b564a9f48dd99e7188d26df01480
Black Ram <67595890+DonRP@users.noreply.github.com>
2023-08-24 17:13:27 +0200

    Report

---
+++ src/components/DRReportGrid.tsx
@@ -0,0 +1,163 @@
+import { AspectRatio, Box, Card, Grid, Typography } from '@mui/joy';
+import { DataGrid } from '@mui/x-data-grid';
+import { DRButtonNoMargin } from './DRButton';
+import DRErrorComponent from './DRErrorComponent';
+
+const columns = [
+    {
+        field: 'device',
+        headerName: 'Device',
+        flex: 1,
+        minWidth: 25,
+        renderCell: (params: any) => (
+            <strong>
+                {params.value?.element &&
+                    <>
+                        {params.value?.element}
+                    </>
+                }
+                {!params.value?.element &&
+                    <>
+                        {params.value?.name}
+                    </>
+                }
+            </strong >
+        ),
+    },
+    {
+        field: 'version',
+        headerName: 'Version',
+        flex: 1,
+        minWidth: 25,
+        renderCell: (params: any) => (
+            <strong>
+                <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+                    {params.value}
+                </Box>
+            </strong >
+        ),
+    },
+    {
+        field: 'download',
+        headerName: 'Download',
+        minWidth: 350,
+        renderCell: (params: any) => (
+            <strong>
+                <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+                    <Grid
+                        container
+                        direction="column"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        <Grid
+                            container
+                            direction="row"
+                            justifyContent="flex-start"
+                            alignItems="center"
+                            spacing={1}
+                        >
+                            <Grid>
+                                {params.value?.mediafire &&
+                                    <DRButtonNoMargin
+                                        label='Mediafire'
+                                        variant="outlined"
+                                        startIcon={
+                                            <img src="https://cdn.worldvectorlogo.com/logos/mediafire-1.svg" width={24} height={24} alt="Logo" />
+                                        }
+                                        onClick={() => {
+                                            window.open(params.value?.mediafire)
+                                        }}
+                                        color='success'
+                                    />
+                                }
+                            </Grid>
+                            <Grid>
+                                {params.value?.mega &&
+                                    <DRButtonNoMargin
+                                        label='Mega'
+                                        variant='soft'
+                                        startIcon={
+                                            <img src="https://seeklogo.com/images/M/mega-icon-logo-75FF6A408B-seeklogo.com.png" width={24} height={24} alt="Logo" />
+                                        }
+                                        onClick={() => {
+                                            window.open(params.value?.mega)
+                                        }}
+                                    />
+                                }
+                            </Grid>
+                            <Grid>
+                                {params.value?.discord &&
+                                    <DRButtonNoMargin
+                                        label='Discord - Supporter'
+                                        variant='soft'
+                                        startIcon={
+                                            <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width={24} height={24} alt="Logo" />
+                                        }
+                                        onClick={() => {
+                                            window.open("https://discord.gg/HFfeJKR")
+                                        }}
+                                    />
+                                }
+                            </Grid>
+                        </Grid>
+                        {params.value?.sha && "SHA1: " + params.value?.sha}
+                    </Grid>
+                </Box>
+            </strong >
+        ),
+    },
+];
+type IReportLink = {
+    mega?: string,
+    mediafire?: string,
+    discord?: boolean,
+    sha?: string,
+}
+type IReportGridRow = {
+    id: number,
+    device: { name: string, element?: JSX.Element }
+    version: string,
+    download: IReportLink,
+}
+type IDRReportGridProps = {
+    title: string,
+    data: IReportGridRow[],
+    logoImage?: string,
+    height?: number,
+    rowHeight?: number,
+}
+
+function DRReportGrid(props: IDRReportGridProps) {
+    const { title, data, logoImage, height = 350, rowHeight = 75 } = props;
+
+    try {
+        return (
+            <Card
+                // elevation={24} 
+                sx={{ minWidth: { xs: 470, sm: 600, md: 900 }, marginTop: 2 }}
+            >
+                <Typography level="title-lg">{title}</Typography>
+                {logoImage &&
+                    <AspectRatio minHeight="120px" maxHeight="200px">
+                        <img
+                            src={logoImage}
+                            alt=""
+                        />
+                    </AspectRatio>
+                }
+                <div style={{ height: height, width: '100%' }}>
+                    <DataGrid
+                        rows={data}
+                        columns={columns}
+                        rowHeight={rowHeight}
+                    />
+                </div>
+            </Card>
+        );
+    } catch (error) {
+        return <DRErrorComponent error={error} text={"DRDownloadGrid"} />
+    }
+}
+
+export default DRReportGrid;
---
+++ src/page/Report.tsx
@@ -0,0 +1,146 @@
+import AndroidIcon from '@mui/icons-material/Android';
+import AppleIcon from '@mui/icons-material/Apple';
+import DesktopWindowsIcon from '@mui/icons-material/DesktopWindows';
+import FolderZipIcon from '@mui/icons-material/FolderZip';
+import { Grid } from '@mui/joy';
+import DRReportGrid from 'components/DRReportGrid';
+import { analyticPageView } from 'utility/Analytics';
+
+const rowsABFD = [
+    {
+        id: 0, device: {
+            name: 'Windows/Linux', element: <>
+                <Grid
+                    container
+                    direction={{ xs: "column", md: "row" }}
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={{ xs: 0, md: 4 }}
+                >
+                    <Grid  >
+                        <DesktopWindowsIcon sx={{ fontSize: 40, display: { xs: 'flex', md: 'none' } }} />
+                        <DesktopWindowsIcon sx={{ fontSize: 30, display: { xs: 'none', md: 'flex' } }} />
+                    </Grid>
+                    <Grid sx={{ fontSize: 15, display: { xs: 'none', md: 'flex' } }} >
+                        Windows/Linux
+                    </Grid>
+                    <Grid sx={{ fontSize: 10, display: { xs: 'flex', md: 'none' } }} >
+                        Windows/Linux
+                    </Grid>
+                </Grid>
+            </>
+        }, version: 'v0.1.1', download: {
+            mega: "https://mega.nz/file/b1tQnKjT#Q0WG8su9jv994F2Hn7u31_I9HkBpDP5afUCsa6lBCfk"
+        }
+    },
+    {
+        id: 1, device: {
+            name: 'Windows/Linux Compress', element: <>
+                <Grid
+                    container
+                    direction={{ xs: "column", md: "row" }}
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={{ xs: 0, md: 4 }}
+                >
+                    <Grid  >
+                        <FolderZipIcon sx={{ fontSize: 40, display: { xs: 'flex', md: 'none' } }} />
+                        <FolderZipIcon sx={{ fontSize: 30, display: { xs: 'none', md: 'flex' } }} />
+                    </Grid>
+                    <Grid sx={{ fontSize: 15, display: { xs: 'none', md: 'flex' } }} >
+                        Windows/Linux Compress
+                    </Grid>
+                    <Grid sx={{ fontSize: 10, display: { xs: 'flex', md: 'none' } }} >
+                        Compress
+                    </Grid>
+                </Grid>
+            </>
+        }, version: 'v0.1.1', download: {
+            mega: "https://mega.nz/file/zwtkUY4L#TTS6XJ7y4trltyaU1qRVeYZT7g3cfhKu0_avGKkczCQ"
+        }
+    },
+    {
+        id: 2, device: {
+            name: 'MacOS', element: <>
+                <Grid
+                    container
+                    direction={{ xs: "column", md: "row" }}
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={{ xs: 0, md: 4 }}
+                >
+                    <Grid  >
+                        <AppleIcon sx={{ fontSize: 40, display: { xs: 'flex', md: 'none' } }} />
+                        <AppleIcon sx={{ fontSize: 30, display: { xs: 'none', md: 'flex' } }} />
+                    </Grid>
+                    <Grid sx={{ fontSize: 15, display: { xs: 'none', md: 'flex' } }} >
+                        MacOS
+                    </Grid>
+                    <Grid sx={{ fontSize: 10, display: { xs: 'flex', md: 'none' } }} >
+                        MacOS
+                    </Grid>
+                </Grid>
+            </>
+        }, version: 'v0.1.1', download: {
+            mega: "https://mega.nz/file/6k0QTYAC#SpxeDO3tuoP_PjhRv08qIgx1FyQfNGPzmUq2ag09Dhg"
+        }
+    },
+    {
+        id: 3, device: {
+            name: 'Android', element: <>
+                <Grid
+                    container
+                    direction={{ xs: "column", md: "row" }}
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={{ xs: 0, md: 4 }}
+                >
+                    <Grid  >
+                        <AndroidIcon sx={{ fontSize: 40, display: { xs: 'flex', md: 'none' } }} />
+                        <AndroidIcon sx={{ fontSize: 30, display: { xs: 'none', md: 'flex' } }} />
+                    </Grid>
+                    <Grid sx={{ fontSize: 15, display: { xs: 'none', md: 'flex' } }} >
+                        Android
+                    </Grid>
+                    <Grid sx={{ fontSize: 10, display: { xs: 'flex', md: 'none' } }} >
+                        Android
+                    </Grid>
+                </Grid>
+            </>
+
+        }, version: 'v0.1.1', download: {
+            mega: "https://mega.nz/file/OkcHVIJa#dOyelMAS49RuBkzmbv2bPBxrxgo4n_wHqQ-63AAbRtY"
+        }
+    },
+];
+
+function Report() {
+    analyticPageView("Download")
+
+    return (
+        <>
+            <Grid
+                container
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                paddingTop={3}
+                paddingBottom={3}
+            >
+                <h2>Report</h2>
+                <DRReportGrid
+                    title="A Big Family in Debit"
+                    data={rowsABFD}
+                    height={411}
+                />
+                <DRReportGrid
+                    title="Web Service"
+                    data={rowsABFD}
+                    height={411}
+                />
+            </Grid>
+        </>
+    );
+}
+
+export default Report;
