81fe79a69c4d9b5eff4061a2dd3a541e2fcd5b51
Black Ram <67595890+DonRP@users.noreply.github.com>
2023-09-05 17:11:11 +0200

    GitHubCreateIssueBody

---
+++ src/model/git/GitHubCreateIssueBody.ts
@@ -0,0 +1,5 @@
+export interface GitHubCreateIssueBody {
+    title: string
+    body: string
+    labels: string[]
+}
--- src/services/GitHubService.ts
+++ src/services/GitHubService.ts
@@ -1,21 +1,24 @@
 import { HttpResponse } from "model/HttpResponse";
+import { GitHubCreateIssueBody } from "model/git/GitHubCreateIssueBody";
 import BaseRestService from "./BaseRestService";
 
 class GitService extends BaseRestService {
-    async createIssue(repo: string, title: string, body = "", labels: string[] = []): Promise<HttpResponse<any>> {
+    async createIssue(repo: string, title: string, bodyIssue = "", labels: string[] = []): Promise<HttpResponse<any>> {
         if (!repo) {
             return new HttpResponse()
         }
 
-        return this.postRequest(this.urlwebapi + `/GitHub/CreateIssue?repositoryName=${repo}`, {
-            title,
-            body,
-            labels
-        })
+        let body: GitHubCreateIssueBody = {
+            "title": title,
+            "body": bodyIssue,
+            "labels": labels,
+        }
+
+        return this.postRequest(this.urlwebapi + `/GitHub/CreateIssue?repositoryName=${repo}`, body)
             .then(response => {
                 if (!response || !response.isSuccessStatusCode || !response.content) {
                     this.showMessage(response?.messagesToShow, 'error')
-                    throw new Error(body);
+                    throw new Error(bodyIssue);
                 }
                 this.showMessage("The issue has been created. Thank you very much.", 'success');
                 return response;
