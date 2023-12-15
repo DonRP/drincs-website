0bd02f80c2981b7b61b8677f0f545c9c0b59bc82
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2023-12-15 15:46:31 +0000

    NarutoShinobiLord

--- src/page/Translations.tsx
+++ src/page/Translations.tsx
@@ -11,6 +11,7 @@ const OtherTranslationProjects = [
     ProjectsEnum.AnotherChance,
     ProjectsEnum.BadMemories,
     ProjectsEnum.BigBrotherAS,
+    ProjectsEnum.NarutoShinobiLord,
     ProjectsEnum.WitchHunter,
 ]
 
