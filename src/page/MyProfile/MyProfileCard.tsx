86757bf847b6cd0f793bf2124064474ca74bd427
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-01-17 12:20:46 +0100

    Refactor MyProfileCard component

--- src/page/MyProfile/MyProfileCard.tsx
+++ src/page/MyProfile/MyProfileCard.tsx
@@ -1,9 +1,9 @@
+import { Card } from '@mui/joy';
 import Box from '@mui/joy/Box';
-import Card from '@mui/joy/Card';
 import CardActions from '@mui/joy/CardActions';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Divider from '@mui/joy/Divider';
-import Stack from '@mui/joy/Stack';
+import DRSheet from 'components/DRSheet';
 
 type Iprops = {
     title: string | JSX.Element,
@@ -19,36 +19,35 @@ export default function MyProfileCard(props: Iprops) {
     } = props
 
     return (
-        <Box sx={{ flex: 1, width: '100%' }}>
-            <Stack
-                spacing={4}
-                sx={{
-                    display: 'flex',
-                    maxWidth: '800px',
-                    mx: 'auto',
-                    px: { xs: 0, md: 6 },
-                    py: { xs: 2, md: 3 },
-                }}
-            >
-                <Card>
-                    <Box sx={{ mb: 1 }}>
-                        {title}
-                    </Box>
-                    <Divider />
-                    <Stack
-                        direction="row"
-                        spacing={3}
-                        sx={{ my: 1 }}
+        <DRSheet
+            sx={{
+                maxWidth: 800,
+                mx: 'auto', // margin left & right
+                my: 4, // margin top & bottom
+                py: 3, // padding top & bottom
+                px: 2, // padding left & right
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+            variant="outlined"
+        >
+            <Card>
+                <Box sx={{ mb: 1 }}>
+                    {title}
+                </Box>
+                <Divider />
+                {body}
+                <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
+                    <CardActions
+                        sx={{
+                            alignSelf: { xs: "self-start", sm: 'flex-end' },
+                            pt: 2
+                        }}
                     >
-                        {body}
-                    </Stack>
-                    <CardOverflow sx={{ borderTop: '1px solid', borderColor: 'divider' }}>
-                        <CardActions sx={{ alignSelf: 'flex-end', pt: 2 }}>
-                            {actions}
-                        </CardActions>
-                    </CardOverflow>
-                </Card>
-            </Stack>
-        </Box>
+                        {actions}
+                    </CardActions>
+                </CardOverflow>
+            </Card>
+        </DRSheet>
     );
 }
