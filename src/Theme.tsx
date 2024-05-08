95d4291d5e6c509ea29a7eba1d957410d784ea96
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-08 10:59:30 +0200

    chore: Update @drincs/react-components to version 0.2.2

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,70 +1,68 @@
-import { extendTheme } from "@drincs/react-components";
+import { createPaletteRange, CssVarsProvider, extendTheme } from "@drincs/react-components";
 import { GlobalStyles } from "@mui/joy";
-import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
-import {
-    THEME_ID as MATERIAL_THEME_ID,
-    Experimental_CssVarsProvider as MaterialCssVarsProvider,
-    experimental_extendTheme as materialExtendTheme
-} from '@mui/material/styles';
 import "./font.css";
 
 declare module '@mui/joy/styles' {
     interface Palette {
+        olther: {
+            gold: string;
+        };
         logo: {
             dicord: string;
             patreon: string;
             patreonDark: string;
         };
+        gold: any;
     }
 
     interface FontFamily {
         sixtyfour: string
     }
+    interface ColorPalettePropOverrides {
+        'gold': true
+    }
 }
 
-const theme = extendTheme({
-    colorSchemes: {
-        light: {
-            palette: {
-                logo: {
-                    dicord: "#7289da",
-                    patreon: "#f96854",
-                    patreonDark: "#052d49",
+export function MyTheme(props: { children: React.ReactNode }) {
+
+    const theme = extendTheme({
+        colorSchemes: {
+            light: {
+                palette: {
+                    olther: {
+                        gold: "#FFD700",
+                    },
+                    logo: {
+                        dicord: "#7289da",
+                        patreon: "#f96854",
+                        patreonDark: "#052d49",
+                    },
+                    gold: createPaletteRange("gold", "#FFD700")
+                },
+            },
+            dark: {
+                palette: {
+                    logo: {
+                        dicord: "#7289da",
+                        patreon: "#f96854",
+                        patreonDark: "#052d49",
+                    },
+                    gold: createPaletteRange("gold", "#FFD700")
                 },
             },
         },
-        dark: {
-            palette: {
-                logo: {
-                    dicord: "#7289da",
-                    patreon: "#f96854",
-                    patreonDark: "#052d49",
-                },
-            },
+        fontFamily: {
+            sixtyfour: "Sixtyfour",
         },
-    },
-    fontFamily: {
-        sixtyfour: "Sixtyfour",
-    },
-});
-
-type MyThemeProps = {
-    children: React.ReactNode;
-}
-
-// https://mui.com/joy-ui/guides/using-joy-ui-and-material-ui-together/
-export function MyTheme(props: MyThemeProps) {
-    const materialTheme = materialExtendTheme();
+    });
 
     return (
-        <MaterialCssVarsProvider
+        <CssVarsProvider
             defaultMode="dark"
-            theme={{ [MATERIAL_THEME_ID]: materialTheme }}
+            themeJoy={theme}
         >
-            <JoyCssVarsProvider defaultMode="dark" theme={theme} >
-                <GlobalStyles styles={{ h1: { color: 'white' }, h2: { color: 'white' }, body: { backgroundColor: '#6c4b73', } }} />
-                {props.children}
-            </JoyCssVarsProvider>
-        </MaterialCssVarsProvider>
+            <GlobalStyles styles={{ h1: { color: 'white' }, h2: { color: 'white' }, body: { backgroundColor: '#6c4b73', } }} />
+            {props.children}
+        </CssVarsProvider>
     );
 }
